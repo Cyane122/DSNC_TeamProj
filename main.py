973276bb65e43ee2a97e5631b4189d80cc5f14ce
cyane122 <cyane122@g.skku.edu>
2023-05-16 15:29:42 +0900

    ver 0.0.6
    added - File/Folder Selection, Move into Folder

--- main.py
+++ main.py
@@ -25,21 +25,22 @@ class File:
     def getName(self):
         return self.name + "." + self.extension
 
+    def getLocation(self):
+        loc = self.name + "." + self.extension
+        file = self
+        while file.prev is not None:
+            file = file.prev
+            t: str = file.name + "." + file.extension
+            loc = t + "\\" + loc
+
+        return loc
+
 
 class Folder(File):
     def __init__(self, name, prev):
         super().__init__(name, prev, "dir")
         self.contents = []
         self.isOpened = False
-
-    def getLocation(self):
-        loc = self.name
-        file = self
-        while file.prev is not None:
-            file = file.prev
-            loc = file.name + "\\" + loc
-
-        return loc
 
     def addFile(self, addFile):
         self.contents.append(addFile)
@@ -135,15 +136,25 @@ def createFolder():
 
 def selectFile():
     select = input("[ Select File/Folder ] Enter the alphabet of the file/folder you want to select: ")
-    if dict[select] is None or dict[select] is not File:
+    print(type(dic[ord(select)]))
+    if dic[ord(select)] is None:
         print("[ Select File/Folder ] Invalid value; Try again please.")
         time.sleep(0.3)
         return None
-    return dict[select]
+    return dic[ord(select)]
+
+
+def copyFileFolder():
+    pass
+
+
+def moveFileFolder():
+    pass
 
 
 if __name__ == "__main__":
     while True:
+        print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
         LIST = ["Create File", "Create Folder"]
         select = 0
 
@@ -156,12 +167,19 @@ if __name__ == "__main__":
                 LIST.append("Move Folder")
             else:
                 LIST.append("Unselect File")
+                if current_dir.contains(selected_dir):
+                    LIST.append(("Goto " + selected_dir.name))
                 LIST.append("Copy File")
                 LIST.append("Move File")
+
+        if current_dir.prev is not None:
+            LIST.append("Move to " + current_dir.prev.name)
 
         print("==========================================")
         print("Python File Manager")
         print("Current Direction: ", current_dir.getLocation())
+        if selected_dir is not None:
+            print("Selected Direction:", selected_dir.getLocation())
         print("==========================================")
         idx: int = 65  # chr(idx) = 'A'
 
@@ -172,7 +190,7 @@ if __name__ == "__main__":
             print("this folder is empty...")
         else:
             for d in current_dir.contents:
-                dic[idx] = d
+                dic[idx]: File = d
                 print(f"[{chr(idx)}] {d.getName():18s}", end="")
                 now = d.time
                 print("%04d/%02d/%02d %02d:%02d:%02d" % (
@@ -198,3 +216,13 @@ if __name__ == "__main__":
             tmp = selectFile()
             if tmp is not None:
                 selected_dir = tmp
+        elif select == "Unselect File" or select == "Unselect Folder":
+            selected_dir = None
+        elif select == "Copy File" or select == "Copy Folder":
+            copyFileFolder()
+        elif select == "Move File" or select == "Move Folder":
+            moveFileFolder()
+        elif selected_dir is not None and select == "Goto " + selected_dir.name:
+            current_dir = selected_dir
+        elif select == "Move to " + current_dir.prev.name:
+            current_dir = current_dir.prev
